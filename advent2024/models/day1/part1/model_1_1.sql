e466c8d95fc55ed6c4df37a02284b6eaa57bdd57
MattiasMTS <mattias.sjodin@northvolt.com>
2024-12-02 19:01:27 +0100

    feat: solve day1

--- advent2024/models/day1/part1/model_1_1.sql
+++ advent2024/models/day1/part1/model_1_1.sql
@@ -1,13 +1,25 @@
 with
-    to_rows as (select regexp_split_to_table(input, '\n') as input from {{ ref("input_1") }}),
-    to_columns as (
+    to_table as (
+        select regexp_split_to_table(input, '\n') as input
+        from {{ ref("input_1") }}
+    ),
+    to_cols as (
         select
-            split_part(input, '   ', 1)::int as column_1,
-            split_part(input, '   ', 2)::int as column_2
-        from to_rows
-        where input != ''
-        order by column_1, column_2
+            split_part(input, '   ', 1)::int as list_1,
+            split_part(input, '   ', 2)::int as list_2
+        from to_table
+    ),
+    to_lists as (
+        select
+            list(list_1 order by list_1) as list_1,
+            list(list_2 order by list_2) as list_2
+        from to_cols
+    ),
+    to_unnest as (
+        select unnest(list_1) as col1, unnest(list_2) as col2
+        from to_lists
     )
-select sum(column_2 - column_1) as answer
-from to_columns
+select sum(abs(col2 - col1)) as answer
+from to_unnest
+
 
--- advent2024/models/day1/part2/model_1_2.sql
+++ advent2024/models/day1/part2/model_1_2.sql
@@ -1,2 +1,36 @@
-select * from {{ ref("input_1") }}
+with
+    to_table as (
+        select regexp_split_to_table(input, '\n') as input
+        from {{ ref("input_1") }}
+    ),
+    to_cols as (
+        select
+            split_part(input, '   ', 1)::int as list_1,
+            split_part(input, '   ', 2)::int as list_2
+        from to_table
+    ),
+    to_lists as (
+        select
+            list(list_1 order by list_1) as list_1,
+            list(list_2 order by list_2) as list_2
+        from to_cols
+    ),
+    to_unnest as (
+        select unnest(list_1) as col1, list_2
+        from to_lists
+    ),
+    list_transform as (
+        select
+            col1,
+            list_transform(list_2, x -> case when x = col1 then 1 end) as list_2
+        from to_unnest
+    ),
+    list_filter as (
+        select
+            col1,
+            list_filter(list_2, x -> x is not null) as list_2
+        from list_transform
+    )
 
+select sum(col1 * len(list_2)) as answer
+from list_filter
