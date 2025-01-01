a3a7e572740df06955e2eae146d719741a2b8fb7
MattiasMTS <mattias.sjodin.6764@hotmail.com>
2025-01-01 16:03:00 +0100

    feat: solve day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,64 @@
+import kotlin.math.abs
+
 fun main() {
+    fun getLists(input: List<String>): Pair<List<Int>, List<Int>> {
+        val list1: MutableList<Int> = mutableListOf()
+        val list2: MutableList<Int> = mutableListOf()
+
+        for (i in input) {
+            val temp: List<String> = i.split("   ")
+            try {
+                val tt1 = temp[0].toInt()
+                val tt2 = temp[1].toInt()
+                list1.add(tt1)
+                list2.add(tt2)
+            } catch (e: NumberFormatException) {
+                println("Error: $e")
+            }
+        }
+
+        return Pair(list1, list2)
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        // sorted lists ascendingly
+        val (list1, list2) = getLists(input)
+
+        val list1Sorted = list1.sorted()
+        val list2Sorted = list2.sorted()
+
+        var total: Int = 0
+
+        for (i in 0..<list1Sorted.size) {
+            val t1 = list1Sorted[i]
+            val t2 = list2Sorted[i]
+
+            total += abs(t2 - t1)
+        }
+
+        return total
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (list1, list2) = getLists(input)
+        var total: Int = 0
+
+        for (i in list1) {
+            var count: Int = 0
+            for (j in list2) {
+                if (i == j) {
+                    count++
+                }
+            }
+            total += count * i
+        }
+
+        return total
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(readInput("Day01_test")) == 11)
+    check(part2(readInput("Day01_test")) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
--- src/Utils.kt
+++ src/Utils.kt
@@ -2,18 +2,20 @@ import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readText
+import kotlin.text.padStart
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readText().trim().lines()
+fun readInput(name: String): List<String> = Path("src/$name.txt").readText().trim().lines()
 
 /**
  * Converts string to md5 hash.
  */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+fun String.md5() =
+    BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+        .toString(16)
+        .padStart(32, '0')
 
 /**
  * The cleaner shorthand for printing output.
